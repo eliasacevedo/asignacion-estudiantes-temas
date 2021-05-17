604e7ecd8de659485d48e0453ce9ee5ddbc9fa07
Elias Acevedo <eliasjoseacevedo61@gmail.com>
2021-05-17 00:53:20 -0400

    Agregando implementacion

--- codigo.console/Program.cs
+++ codigo.console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using codigo.produccion.Equipo;
 
 namespace codigo.console
 {
@@ -6,7 +8,20 @@ namespace codigo.console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ec = new EquipoConstructor();
+            ec.Temas = new List<string>() {
+                "A", "B", "C", "D", "E", "F", "G", "H", "I","J","K","L","M"
+            };
+            ec.Estudiantes = new List<string>() {
+                "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9",
+                "E10", "E20", "E30", "E40", "E50", "E60", "E70", "E80", "E90",
+                "E110", "E210", "E130", "E140", "E510", "E601", "E710", "E810", "E901"
+            };
+
+            ec.GenerarEquipos(9);
+            ec.AsignarTemas();
+            
+            Console.Write(ec.Equipos);
         }
     }
 }
